0a6b4446e80df99e5b98290000914bf93a4ae687
noiach <gh@noiach.com>
2015-07-04 11:52:52 +1000

    More icons

--- app/views.py
+++ app/views.py
@@ -8,17 +8,19 @@ def index():
     user = {'nickname': '***REMOVED***'}
     actions = {
             "fire": { "name": "there is a fire within 10km of me", "icon": "fire.svg", "color":"#C74350"},
-            "recycle": { "name": "something about recyling", "icon": "logo.svg","color":"#C74350"},
-            "animals": { "name": "theres a new daily animal fact", "icon": "animal.svg", "color":"#C74350"}
+            "recycle": { "name": "something about recyling", "icon": "logo.svg","color":"#9F367A"},
+            "animals": { "name": "theres a new daily animal fact", "icon": "animal.svg", "color":"#C74350"},
             }
 
     reactions = {
-            "email": { "name": "email me", "icon": "email.svg", "color":"#9DC241"}
+            "email": { "name": "email me", "icon": "email.svg", "color":"#9DC241"},
+            "sms": { "name": "send me an SMS", "icon": "sms.svg", "color":"#2B8175"},
             }
 
     my_recipes = [
             { "action": "fire", "reaction": "email", "id": 0 },
-            { "action": "recycle", "reaction": "email", "id": 0 }
+            { "action": "recycle", "reaction": "sms", "id": 2 },
+            { "action": "recycle", "reaction": "email", "id": 1 },
             ]
 
     return render_template( 'index.html',
