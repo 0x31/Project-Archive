e4c48131b33e507a68660bacbb411b8004922ce5
noiach <gh@noiach.com>
2015-08-20 09:24:44 +1000

    Added main class and A.

---
+++ src/gittest/A.java
@@ -0,0 +1,11 @@
+package gittest;
+
+/**
+ * Created by nosha on 20/08/15.
+ */
+public class A {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ src/gittest/Main.java
@@ -0,0 +1,10 @@
+package gittest;
+
+/**
+ * Created by ***REMOVED*** on 20/08/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
