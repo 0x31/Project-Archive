44711be5a7b12985d6bf74a2a3f2b222bf61c2a1
Timothy David Crundall <u5018130@anu.edu.au>
2015-08-20 10:20:14 +1000

    reference pseudo code by Tutor

---
+++ src/gittest/Pieces.java
@@ -0,0 +1,25 @@
+package gittest;
+
+/**
+ * Created by ***REMOVED*** on 20/08/15.
+ */
+public enum Pieces {
+    A(List(0,1,2,3,4,5,)),
+    B,
+    C,
+    D;
+
+    private List<positions> postition;
+
+    private Pieces(List<Positions>) {
+        this.position = postition;
+    }
+
+    public List<Postions> getOccupiedSpaces() {
+       return positions;
+    }
+
+    public rotatePiece() {
+
+    }
+}
