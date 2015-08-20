f04de06f7934aa315778ddd02f94323c64a0dba3
Timothy David Crundall <u5018130@anu.edu.au>
2015-08-20 10:07:12 +1000

    type corrected

--- src/gittest/Main.java
+++ src/gittest/Main.java
@@ -6,7 +6,7 @@ package gittest;
 public class Main {
     public static void main(String[] args) {
         A a = new A();
-        B b = new B{};
+        B b = new B();
         C c = new C();
     }
 }
