1e0a3b05df8e95f6ddae3fa29e2a7860ab9f8045
Timothy David Crundall <u5018130@anu.edu.au>
2015-08-22 20:11:41 +1000

    minor tidying

---
+++ tests/comp1140/ass2/PiecesTest.java
@@ -0,0 +1,17 @@
+package comp1140.ass2;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by ***REMOVED*** on 22/08/15.
+ */
+public class PiecesTest {
+    public void checkGetCoordinates() {
+        TimsPiece myPiece = new TimsPiece(Shape.T);
+
+        Coordinate[] is     = myPiece.getCoordinates(new Coordinate(0,0),0);
+        Coordinate[] should = {new Coordinate( 0, 0), new Coordinate( 0, 1), new Coordinate( 1, 1), new Coordinate( 2, 1), new Coordinate( 1, 2)};
+        assertTrue("Error: getCoordinatesHasFailed", is.equals(should));
+    }
+}
